74bbeea31dc527dc3a38d7af373c0b49388f5732
bennjr <benjaminreinertsen@gmail.com>
2025-08-27 00:25:32 +0200

    Updated with new command, still not working

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-const {Client, GatewayIntentBits, Collection} = require("discord.js")
-const {Client, GatewayIntentBits, Collection, REST, Routes} = require("discord.js")
-const fs = require("fs")
-const path = require("path")
+const { Client, GatewayIntentBits, Collection, REST, Routes } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const TEMP_KEY = JSON.parse(fs.readFileSync("server/config.json")).API_KEY
 
 const client = new Client({
     intents: [
@@ -12,16 +13,8 @@ const client = new Client({
     ]
 })
 
-client.commands = new Collection()
-const commandPath = path.join(__dirname, "commands")
-const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith(".js"))
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandPath, file)
-    const command = require(filePath)
-    client.commands.set(command.name, command)
-}
-
-
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
 
 client.login(TEMP_KEY)
---
+++ commands/forums.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, ChannelType } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("Save")
+        .setDescription("Begin saving process")
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription("Save recording to forum")
+                .setRequired(true)
+                .addChannelTypes(ChannelType.GuildForum)),
+
+    async execute(interaction) {
+        const channel = interaction.options.getChannel('channel');
+        console.log(`Selected channel: ${channel.name}`);
+    }
+}
