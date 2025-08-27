6ddced784661c975236b9f3981a1c508307e3c89
bennjr <benjaminreinertsen@gmail.com>
2025-08-27 23:55:37 +0200

    Updateed main bot.js file with functionality

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const TEMP_KEY = JSON.parse(fs.readFileSync("server/config.json")).API_KEY
+const GUILD_ID = JSON.parse(fs.readFileSync("server/config.json")).GUILD_ID
 
 const client = new Client({
     intents: [
@@ -13,8 +14,48 @@ const client = new Client({
     ]
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+client.slashCommands = new Collection()
+
+const commandsPath = path.join(__dirname, "commands")
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"))
+const commands = []
+
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    if (command.data) {
+        client.slashCommands.set(command.data.name, command)
+        commands.push(command.data.toJSON())
+    }
+}
+
+client.once("ready", async () => {
+    console.log(`Logged in as ${client.user.tag}!`)
+
+    const rest = new REST().setToken(TEMP_KEY)
+
+    try {console.log("Refreshing application (/) commands.")
+        await rest.put(
+            Routes.applicationGuildCommands(client.user.id, GUILD_ID),
+            { body: commands }
+        )
+        console.log("Reloaded application (/) commands.")
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand()) return
+
+    const command = client.slashCommands.get(interaction.commandName)
+    if (!command) return
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+        console.error(error)
+        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true })
+    }
 })
 
 client.login(TEMP_KEY)
